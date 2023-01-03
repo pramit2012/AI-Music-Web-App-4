7055eb9e6ad593492f5a940ab92446ec5a04444c
pramit2012 <101552755+pramit2012@users.noreply.github.com>
2023-01-03 20:02:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+song1 = ""; 
+song2 = ""; 
+song1_status = ""; 
+song2_status = ""; 
+scoreRightWrist = 0; 
+scoreLeftWrist = 0; 
+rightWristX = 0; 
+rightWristY = 0; 
+leftWristX = 0; 
+leftWristY = 0; 
+function preload() { 
+    song1 = loadSound("music.mp3"); 
+    song2 = loadSound("music2.mp3"); 
+}
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+    fill('#FF0000');
+    stroke('#FF0000');
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song1.stop();
+        if (song2_status == false) {
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing - Peter Pan song";
+        }
+    }
+}
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
